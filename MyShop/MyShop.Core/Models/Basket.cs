4bcb1195552feb604008db275326698eac6443b1
Akram AlOrjany <akram.orj.96@gmail.com>
2019-06-18 12:03:21 +0300

    Added Basket

---
+++ MyShop/MyShop.Core/Models/Basket.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace MyShop.Core.Models
+{
+    public class Basket : BaseEntity
+    {
+        public virtual ICollection<BasketItem> BasketItems { get; set; }
+
+        public Basket()
+        {
+            BasketItems = new List<BasketItem>();
+        }
+    }
+}
