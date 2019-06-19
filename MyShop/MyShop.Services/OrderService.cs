b555fc3f081cd132892d46b174a8319a34ae1e30
Akram AlOrjany <akram.orj.96@gmail.com>
2019-06-19 14:13:56 +0300

    Order Manager Controller

--- MyShop/MyShop.Services/OrderService.cs
+++ MyShop/MyShop.Services/OrderService.cs
@@ -1,7 +1,11 @@
 ﻿using MyShop.Core.Contracts;
 using MyShop.Core.Models;
-using MyShop.Core.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyShop.Core.ViewModels;
 
 namespace MyShop.Services
 {
@@ -10,7 +14,7 @@ namespace MyShop.Services
         IRepository<Order> orderContext;
         public OrderService(IRepository<Order> OrderContext)
         {
-            orderContext = OrderContext;
+            this.orderContext = OrderContext;
         }
 
         public void CreateOrder(Order baseOrder, List<BasketItemViewModel> basketItems)
@@ -28,6 +32,22 @@ namespace MyShop.Services
             }
 
             orderContext.Insert(baseOrder);
+            orderContext.Commit();
+        }
+
+        public List<Order> GetOrderList()
+        {
+            return orderContext.Collection().ToList();
+        }
+
+        public Order GetOrder(string Id)
+        {
+            return orderContext.Find(Id);
+        }
+
+        public void UpdateOrder(Order updatedOrder)
+        {
+            orderContext.Update(updatedOrder);
             orderContext.Commit();
         }
     }
