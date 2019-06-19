809ede01405faf3aff06ce0207aa74f615c974c2
Akram AlOrjany <akram.orj.96@gmail.com>
2019-06-19 13:10:52 +0300

    Linked Customer to order

--- MyShop/MyShop.WebUI/Controllers/BasketController.cs
+++ MyShop/MyShop.WebUI/Controllers/BasketController.cs
@@ -1,64 +1,94 @@
 ﻿using MyShop.Core.Contracts;
 using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 
 namespace MyShop.WebUI.Controllers
 {
     public class BasketController : Controller
     {
+        IRepository<Customer> customers;
         IBasketService basketService;
         IOrderService orderService;
 
-        public BasketController(IBasketService BasketService, IOrderService OrderService)
+        public BasketController(IBasketService BasketService, IOrderService OrderService, IRepository<Customer> Customers)
         {
-            basketService = BasketService;
-            orderService = OrderService;
+            this.basketService = BasketService;
+            this.orderService = OrderService;
+            this.customers = Customers;
         }
         // GET: Basket2
         public ActionResult Index()
         {
-            var model = basketService.GetBasketItems(HttpContext);
+            var model = basketService.GetBasketItems(this.HttpContext);
             return View(model);
         }
 
         public ActionResult AddToBasket(string Id)
         {
-            basketService.AddToBasket(HttpContext, Id);
+            basketService.AddToBasket(this.HttpContext, Id);
 
             return RedirectToAction("Index");
         }
 
         public ActionResult RemoveFromBasket(string Id)
         {
-            basketService.RemoveFromBasket(HttpContext, Id);
+            basketService.RemoveFromBasket(this.HttpContext, Id);
 
             return RedirectToAction("Index");
         }
 
         public PartialViewResult BasketSummary()
         {
-            var basketSummary = basketService.GetBasketSummary(HttpContext);
+            var basketSummary = basketService.GetBasketSummary(this.HttpContext);
 
             return PartialView(basketSummary);
         }
 
+        [Authorize]
         public ActionResult Checkout()
         {
-            return View();
+            Customer customer = customers.Collection().FirstOrDefault(c => c.Email == User.Identity.Name);
+
+            if (customer != null)
+            {
+                Order order = new Order()
+                {
+                    Email = customer.Email,
+                    City = customer.City,
+                    State = customer.State,
+                    Street = customer.Street,
+                    FirstName = customer.FirstName,
+                    Surname = customer.LastName,
+                    ZipCode = customer.ZipCode
+                };
+
+                return View(order);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+
         }
 
         [HttpPost]
+        [Authorize]
         public ActionResult Checkout(Order order)
         {
 
-            var basketItems = basketService.GetBasketItems(HttpContext);
+            var basketItems = basketService.GetBasketItems(this.HttpContext);
             order.OrderStatus = "Order Created";
+            order.Email = User.Identity.Name;
 
             //process payment
 
             order.OrderStatus = "Payment Processed";
             orderService.CreateOrder(order, basketItems);
-            basketService.ClearBasket(HttpContext);
+            basketService.ClearBasket(this.HttpContext);
 
             return RedirectToAction("Thankyou", new { OrderId = order.Id });
         }
